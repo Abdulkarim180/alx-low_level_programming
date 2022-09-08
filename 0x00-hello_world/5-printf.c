ff2408ea3c3e3e3c03dc263cc3634e663ed008a1
“Abdulkarim180” <mohammedabdulkarim180@gmail.com>
2022-09-08 15:42:36 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+nclude <stdio.h>                                                                                     
+/**                                                                                                    
+ *  * main - print string in put function                                                                 
+ *   *                                                                                                     
+ *    * Description: using the main function                                                                
+ *     * this programme prints "Programming is like building a multilingual puzzle                           
+ *      * Return: 0                                                                                           
+ *       */                                                                                                    
+int main(void)                                                                                         
+{                                                                                                      
+	printf("\"Programming is like building a multilingual puzzle");                                          
+	return (0);                                                                                            
+}                
