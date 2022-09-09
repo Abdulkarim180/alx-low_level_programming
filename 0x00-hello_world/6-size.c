278974b2d33e10b914a3bcfca7e16cb8f47fd7c3
Abdulkarim180 <111910825+Abdulkarim180@users.noreply.github.com>
2022-09-09 01:15:31 +0000

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@ int main(void)
 int i;
 long li;
 long long lli;
- float f;
+float f;
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
