ca326ef38f549b2769ca4d7fcb099d967f2a4fdf
Abdulkarim180 <111910825+Abdulkarim180@users.noreply.github.com>
2022-09-09 23:02:22 +0000

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
 * main - Prints numbers between 00 to 99.
- *
- * Return: Always 0 (Success)
- */
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int i, e, g, h, op1, op2;
