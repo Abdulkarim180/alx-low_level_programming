ee9cf5fe9b27d95150726235772f4dde1421bcff
“Abdulkarim180” <mohammedabdulkarim180@gmail.com>
2022-09-09 04:27:01 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always (success/correct)
+ */
+int main(void)
+{
+	char alphabets;
+
+	for (alphabets = 122; alphabets >= 97; alphanbets--)
+	{
+		putchar(alphabets);
+	}
+	putchar('\n');
+	return (0);
+}
