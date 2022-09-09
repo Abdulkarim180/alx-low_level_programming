68db611fb60eb6b67c970ea8be3166eba374eb8d
“Abdulkarim180” <mohammedabdulkarim180@gmail.com>
2022-09-09 04:01:00 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always (success/correct)
+ */
+int main(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		if (alphabets == 'q')
+			continue;
+
+		else if (alphabets == 'e')
+			continue;
+	}
+		putchar('\n')
+		return (0);
+}
