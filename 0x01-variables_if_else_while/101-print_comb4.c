bfc2a3afdf160d094f3ba63b4ba3ca39f012c36c
Abdulkarim180 <111910825+Abdulkarim180@users.noreply.github.com>
2022-09-09 23:34:26 +0000

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,33 +1,44 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Prints numbers between 012 to 789.
  *
- * codes for printing all possible combination of two d/t digit
- * in ascending order and seperated by a comma followed by space
- *
- * Return: always (success/correct)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int digit1, digit2, digit3;
+	int i, e, g;
 
-for (digit1 = 0; digit1 < 9; digit1++)
-{
-for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-{
-for (digit3 = digit2 + 1; digit3 < 10; digit3++)
-{
-putchar((digit1 % 10) + '0');
-putchar(digit2 % 10) + '0');
-putchar(digit3 % 10) + '0');
+	i = 48;
+	e = 48;
+	g = 48;
 
-if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-continue;
-putchar(',');
-putchar(' ');
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			g = 48;
+			while (g < 58)
+			{
+				if (e != i && e != g && i != g && e < i && i < g)
+				{
+					putchar(e);
+					putchar(i);
+					putchar(g);
+					if (i == 56 && e == 55 && g == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
 				}
-}
-}
-putchar('\n');
-return (0);
+				g++;
+			}
+			i++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
 }
