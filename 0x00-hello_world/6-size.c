b7b0c4223632e4336b68bef8d795d007f5c714a6
Abdulkarim180 <111910825+Abdulkarim180@users.noreply.github.com>
2022-09-09 00:47:45 +0000

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,15 @@
+6-size.c
 #include <stdio.h>
 /**
- * main - print the sting in the puts function
+ * main - Prints sized of characters
  *
- * Description:using main function
- * Return: 0
+ *Return: Always 0 (success)
  */
 int main(void)
 {
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
-
-	printf("size of char: %d byte(s)\n", sizeof(c));
-	printf("size of int: %d byte(s)\n", sizeof(i));
-	printf("size of long: %d byte(s)\n", sizeof(li));
-	printf("size of long long: %d byte(s)\n", sizeof(lli));
-	printf("size of float: %d byte(s)\n", sizeof(f));
-	return (0);
-}
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
+return (0)
