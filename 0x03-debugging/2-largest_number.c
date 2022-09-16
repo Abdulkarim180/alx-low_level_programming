9da27fe58a5a1bafcd0846fd156838282a331091
Abdulkarim180 <111910825+Abdulkarim180@users.noreply.github.com>
2022-09-16 04:06:49 +0000

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,6 @@
-#include "main.h"
+[2:08 am, 16/09/2022] +233 20 855 0500: #include "main.h"
+
+
 
 /**
  * largest_number - returns the largest of 3 numbers
@@ -8,11 +10,47 @@
  * Return: largest number
  */
 
-int largest_number(int a, int b, int c)
-{
-	int largest;
 
-	largest = (a > b) ? a : b;
-	largest = (largest > c) ? largest : c;
-	return (largest);
+
+int largest_number(int a, int b, int c)
+
+{
+
+    int largest;
+
+
+
+    if (a >= b && a >= c)
+    {
+        largest = a;
+    }
+    else if (b >= a && b >= c)
+    {
+        largest = b;
+    }
+
+    else if (c >= a && c >= b)
+    {
+        largest = c;
+    }
+
+    return (largest);
+
+}
+[2:08 am, 16/09/2022] +233 20 855 0500: 0-main.c
+[2:08 am, 16/09/2022] +233 20 855 0500: #include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0 always success
+ */
+int main(void)
+{
+int i;
+
+i = 0;
+
+positive_or_negative(i);
+
+return (0);
 }
