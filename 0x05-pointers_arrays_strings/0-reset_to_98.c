a5f3ebd419cb6219f44b09582891ee147617017f
“Abdulkarim180” <mohammedabdulkarim180@gmail.com>
2022-09-19 15:20:41 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - returning function
+ * reset_to_98 - a function that points to an int and updates it value to 98
+ * @n:
+ * Return: nothing
+ */
+int main(void)
+{
+	reset_to_98(int *n);
+
+	*n = 98;
+}
