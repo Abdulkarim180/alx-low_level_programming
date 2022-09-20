9be8fc9d880ebb4c28dfd86e579948cc58416c37
“Abdulkarim180” <mohammedabdulkarim180@gmail.com>
2022-09-20 14:08:36 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,18 @@
+#include <stdio.h>
 #include "main.h"
-
 /**
+ * _puts - prints a string
+ * @str: string to print
  *
+ * Description: prints a string
+ * On success: returns no error
+ */
+void _puts(char *str)
+{
+	while (*(str + i) != '\0')
+	{
+	putchar(*(str + i));
+	i++;
+	}
+	putchar(10);
+}
